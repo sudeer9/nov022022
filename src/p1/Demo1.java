ec011ee9b8d35aeb1d9de0226ad4416f1f030922
sudeerkumara.s <sudeerkumara.s@BRILAP2989.BRILLIO.COM>
2022-11-02 07:52:56 +0530

    Modification and new file creation

--- src/p1/Demo1.java
+++ src/p1/Demo1.java
@@ -1,10 +1,10 @@
 package p1;
 
-public class Demo {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("demo");
+		System.out.println("demo1");
 
 	}
 
